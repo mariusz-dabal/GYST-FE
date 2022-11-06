4efdf9d0f05505afa7a21006e661718772a364a7
mariusz-dabal <mariusz9dabal@gmail.com>
2022-11-06 19:02:19 +0100

    Init

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app>
+    <v-main>
+      <Navbar />
+      <v-container>
+        <router-view></router-view>
+        <router-link to="/">Home</router-link>
+        <router-link to="/about">About</router-link>
+      </v-container>
+      <Footer app />
+    </v-main>
+  </v-app>
+</template>
+
+<script setup>
+  import Navbar from '@/components/Navbar.vue'
+  import Footer from '@/components/Footer.vue'
+</script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,34 @@
+<template>
+    <v-footer
+      color="primary"
+      class="text-center d-flex flex-column"
+    >
+      <div>
+        <v-btn
+          v-for="icon in icons"
+          :key="icon"
+          class="mx-4"
+          :icon="icon"
+          variant="text"
+        ></v-btn>
+      </div>
+  
+      <v-divider></v-divider>
+  
+      <div>
+        {{ new Date().getFullYear() }} — <strong>GYST</strong>
+      </div>
+    </v-footer>
+</template>
+<script>
+    export default {
+      data: () => ({
+        icons: [
+          'mdi-facebook',
+          'mdi-twitter',
+          'mdi-linkedin',
+          'mdi-instagram',
+        ],
+      }),
+    }
+</script>
